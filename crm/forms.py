8b9e6e2650d4f8a46c4d1442c28833f77077b5c5
Burnnet <Burnnet00@mail.ru>
2023-05-28 17:36:05 +0300

    add label form

--- crm/forms.py
+++ crm/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 
 class OrdersForm(forms.Form):
-    name = forms.CharField(max_length=200, required=False, widget=forms.TextInput(attrs={'class': 'css_input'}))#добавимо необовязкове поле, виджет створеного цсс
-    phone = forms.CharField(max_length=200)
+    name = forms.CharField(label='Імя' ,max_length=200, required=False, widget=forms.TextInput(attrs={'class': 'css_input'}))#добавимо необовязкове поле, виджет створеного цсс
+    phone = forms.CharField(label='Телефон', max_length=200)
+
+
 
